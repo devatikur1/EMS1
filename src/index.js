77e9ac716b6e1923991e704b8b7ac7dc040293b5
Atikur Rahman <fulldevatikur@gmail.com>
2025-12-17 22:32:51 +0600

    feat: initialize React application entry point with `AppContextProvider` and Tailwind CSS.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./Tailwind.css";
 import App from "./App";
+import AppContextProvider from "./context/AppContextProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </React.StrictMode>
 );
